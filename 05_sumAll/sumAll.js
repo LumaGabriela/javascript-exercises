943456aa0b9f8e8991952f114f3bd24785517ffd
LumaGabriela <lumagabriela3331@gmail.com>
2021-08-24 15:48:15 -0300

    update

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let numbers = []
+    let sum = 0
 
+    if(a>=0 && b>=0 && typeof a === 'number' && typeof b === 'number'){
+        let k = 0
+        if(a>b){
+            k=a-b
+            for(let i=k; i>=0; i--){
+                numbers[i] = a - i
+                console.log('numbers[i]: ' + numbers[i])
+                //console.log(k)
+                sum += numbers[i]
+            }
+        }else{
+            k=b-a
+            for(let i=0; i<=k; i++){
+                numbers[i] = a + i
+                console.log('numbers[i]: ' + numbers[i])
+                //console.log(k)
+                sum += numbers[i]
+            }
+        }
+        
+        return sum
+    }else { 
+        return 'ERROR'
+    }
 };
-
+console.log(sumAll(13,'10'))
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    if(typeof year ==='number' && year%4 === 0 ){
+        if(year%100===0 && year%400!==0){
+            return false
+        }else{
+            return true
+        }
+    }else{
+        return false
+    }
+}
+console.log(leapYears(700))
 module.exports = leapYears;
