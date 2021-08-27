22854e845859cc6a19e12939af5aa3e5dc98f977
LumaGabriela <lumagabriela3331@gmail.com>
2021-08-26 22:27:20 -0300

    09 concluded

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,52 @@
-const add = function() {
-	
+
+const add = function(...arr) {
+	let numbers =[]
+  let result = 0
+  arr.forEach((a,i)=> {numbers[i] = a})
+  numbers.forEach((a,i) =>{result += numbers[i]; console.log(numbers[i])})
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  let result = a-b
+  console.log(result)
+  return result  
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  
+  let result = 0
+  
+  numbers.forEach((a,i) => {result += numbers[i]}) 
+  console.log(numbers)
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+   let result = 1
+   numbers.forEach((a,i) => {result  *= a})
+   return result
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = a**b
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	result = 1
+  if(x>0){
+    for(let i=0; i<x; i++){
+      result *= (x-i) 
+      console.log(x-i)
+    }return result
+  }else if(x==0){
+    return result
+  }else{
+    return false
+  }
 };
-
+console.log(factorial(0))
 module.exports = {
   add,
   subtract,
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+let regex = /[a-z]/i
+const palindromes = function (w) {
+    let result =''
+    let first = '' 
+    let word = w.toLowerCase()
+    for(i=0; i<word.length; i++){
+        if(regex.test(word[i])){
+            result += word[i]
+            first += word[i]
+    } 
+}   let newRes = []
+    newRes = result.split("")
+    newRes.reverse()
+    newRes = newRes.join("")
+    if(newRes === first){
+        return true
+    }else{
+        return false
+    }
 };
-
+console.log(palindromes('Racecar!'))
 module.exports = palindromes;
