cbeca74bc49bb68b87944a2309cf3a772ad638cb
LumaGabriela <lumagabriela3331@gmail.com>
2021-08-28 15:51:55 -0300

    finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,29 +1,43 @@
-const findTheOldest = function() {
-    let age = []
-    const people = [
-        {
-            name:'Carly',
-            yearOfBirth: 1942,
-            yearOfDeath: 1970,
-        },
-        {
-            name:'Ray',
-            yearOfBirth: 1962,
-            yearOfDeath: 2011,
-        },
-        {
-            name: "Jane",
-            yearOfBirth: 1912,
-            yearOfDeath: 1941,
-          },
-    ]
-    const findPerson = (array, name) =>{
-        const returnName = array.find((person, i) => {
-            return person.name === name
+const people = [
+    {
+        name:'Carly',
+        yearOfBirth: 1942,
+        yearOfDeath: 1990,
+    },
+    {
+        name:'Ray',
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+]
+const findTheOldest = function(arr) {
+    let oldest = ''
+    let age = 0
+    const findPerson = (array, actual ) =>{
+        const returnName = array.find((person) => {
+            if(!person.yearOfDeath){
+                person.yearOfDeath = new Date().getFullYear()
+                person.age = person.yearOfDeath - person.yearOfBirth
+            }else{
+                person.age = person.yearOfDeath - person.yearOfBirth
+            }
+
+            if (person.age > age){
+                age = person.age
+                oldest = person
+                //console.log(age)
+            }
+            console.log(person.yearOfDeath)
         })
-        return returnName
+        return oldest
+        
     }
-    return findPerson(people,'Ray')
+    return findPerson(arr)
 }
-console.log(findTheOldest())
+console.log(findTheOldest(people))
 module.exports = findTheOldest;
